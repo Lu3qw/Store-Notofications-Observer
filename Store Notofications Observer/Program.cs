8a9af25a40e05b33925c07aad1df6b4575eb67e7
Ostap <gerasimovicostap@gmail.com>
2024-10-26 09:22:28 +0200

    Added class "Store" and code to file "Program.cs"

--- Store Notofications Observer/Program.cs
+++ Store Notofications Observer/Program.cs
@@ -1,1 +1,20 @@
-﻿
+﻿using Store_Notofications_Observer;
+
+Store store = new Store();
+
+Customer oleg = new Customer("Олег");
+Customer viktoria = new Customer("Вікторія");
+
+// Олег та Вікторія підписуються на категорії
+oleg.Subscribe(store, "Смартфони");
+oleg.Subscribe(store, "Ноутбуки");
+viktoria.Subscribe(store, "Смартфони");
+
+// Сповіщення про знижку на "Ноутбуки"
+store.NotifyDiscounts("Ноутбуки", "Знижка 20% на всі ноутбуки!");
+
+// Олег відписується від "Ноутбуки"
+oleg.Unsubscribe(store, "Ноутбуки");
+
+// Сповіщення про знижку на "Смартфони"
+store.NotifyDiscounts("Смартфони", "Знижка 15% на всі смартфони!");
---
+++ Store Notofications Observer/Store.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_Notofications_Observer
+{
+    internal class Store
+    {
+        private DiscountNotification discountNotification;
+
+        public Store()
+        {
+            discountNotification = new DiscountNotification();
+        }
+
+        public void AddSubscriber(string category, ICustomer customer)
+        {
+            discountNotification.AddSubscriber(category, customer);
+        }
+
+        public void RemoveSubscriber(string category, ICustomer customer)
+        {
+            discountNotification.RemoveSubscriber(category, customer);
+        }
+
+        public void NotifyDiscounts(string category, string discountDetails)
+        {
+            discountNotification.Notify(category, discountDetails);
+        }
+    }
+}
