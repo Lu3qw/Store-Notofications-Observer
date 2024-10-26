9471cd34e8226749f2b3db7f0578de373b6bcf90
Ostap <gerasimovicostap@gmail.com>
2024-10-26 09:20:40 +0200

    Added class DiscountNotification

---
+++ Store Notofications Observer/DiscountNotification.cs
@@ -0,0 +1,38 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Store_Notofications_Observer
+{
+    internal class DiscountNotification
+    {
+        private Dictionary<string, List<ICustomer>> subscribers;
+
+        public DiscountNotification()
+        {
+            subscribers = new Dictionary<string, List<ICustomer>>();
+        }
+
+        public void AddSubscriber(string category, ICustomer customer)
+        {
+            if (!subscribers.ContainsKey(category))
+            {
+                subscribers[category] = new List<ICustomer>();
+            }
+            if (!subscribers[category].Contains(customer))
+            {
+                subscribers[category].Add(customer);
+            }
+        }
+
+        public void RemoveSubscriber(string category, ICustomer customer)
+        {
+            if (subscribers.ContainsKey(category))
+            {
+                subscribers[category].Remove(customer);
+            }
+        }
+    }
+}
